a18356d231125db3817a9c9d9d59061eaa233c08
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-02-20 11:49:00 -0500

    Added simple ner.train() and test script in run.py

--- code/ner.py
+++ code/ner.py
@@ -1,6 +1,3 @@
-# Reference:
-# http://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
-
 import torch
 import torch.autograd as autograd
 from torch.autograd import Variable
@@ -9,27 +6,40 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-
-
 class ner(nn.Module):
-	def __init__(self, embedding_dim, hidden_dim, cell_dim, vocab_size,
-		tagset_size, minibatch_size = 1):
+	def __init__(self,
+		embedding_dim, hidden_dim, cell_dim,
+		vocab_size, type_size,
+		learning_rate = 0.1, minibatch_size = 1,
+		max_epoch = 300,
+		train_data = None):
 		super(ner, self).__init__()
+		self.embedding_dim = embedding_dim
 		self.hidden_dim = hidden_dim
 		self.cell_dim = cell_dim
-		slef.minibatch_size = minibatch_size
-		self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
-		self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+		self.vocab_size = vocab_size
+		self.type_size = type_size
+		self.learning_rate = learning_rate
+		self.minibatch_size = minibatch_size
+		self.max_epoch = max_epoch
+		self.train_data = train_data
+
+		self.word_embedding = nn.Embedding(self.vocab_size,
+			self.embedding_dim)
+		self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim)
 
 		# The linear layer that maps from hidden state space to type space
-		self.hidden2type = nn.Linear(hidden_dim, type_set_size)
-		
+		self.hidden2type = nn.Linear(self.hidden_dim, self.type_size)
+
+		self.hidden, self.cell = self.init_hidden_cell()
+
+
+	def init_hidden_cell(self):
 		# Initialize hidden state h_0 and cell state c_0
 		# The axes semantics are (num_layers, minibatch_size, hidden_dim)
-		self.hidden = Variable(torch.zeros(1, self.minibatch_size,
-			self.hidden_dim))
-		self.cell = Variable(torch.zeros(1, self.minibatch_size,
-			self.hidden_dim))
+		return (
+			Variable(torch.zeros(1, self.minibatch_size, self.hidden_dim)),
+			Variable(torch.zeros(1, self.minibatch_size, self.hidden_dim)))
 
 
 	def forward(self, sentence):
@@ -44,9 +54,26 @@ class ner(nn.Module):
 		return type_score
 
 
+	def train(self):
+		loss_function = nn.NLLLoss()
+		# Note that here we called nn.Module.parameters()
+		optimizer = optim.SGD(self.parameters(), lr = self.learning_rate)
+
+		for epoch in range(self.max_epoch):
+			for sen, typ in self.train_data:
+				# Always clear the gradients before use
+				self.zero_grad()
+
+				# Clear the hidden and cell states
+				self.hidden, self.cell = self.init_hidden_cell()
+
+				type_score = self.forward(sen)
+				loss = loss_function(type_score, typ)
+				loss.backward()
+				optimizer.step()
 
 
-
+#	def write_log
 
 
 
--- code/run.py
+++ code/run.py
@@ -5,12 +5,54 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-import ner
+from ner import ner
 
 
-# Script to run the experiments
+def main():
+	# Temporarily generate data by hand for test purpose
+	training_data = [
+		("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+		("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+	]
+	word_to_ix = {}
+	for sent, tags in training_data:
+		for word in sent:
+			if word not in word_to_ix:
+				word_to_ix[word] = len(word_to_ix)
+	print(word_to_ix)
+
+	tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
+	print(tag_to_ix)
+
+	def prepare_sequence(seq, to_ix):
+		idxs = [to_ix[w] for w in seq]
+		tensor = torch.LongTensor(idxs)
+		return Variable(tensor)
+
+	training_data_in_index = [(prepare_sequence(sen, word_to_ix), prepare_sequence(typ, tag_to_ix)) for sen, typ in training_data]
+	"""
+	for idx, (sen, typ) in enumerate(training_data_in_index):
+		print("data", idx)
+		print(sen)
+		print(typ)
+	"""
+
+
+	######################################
+	embedding_dim = 6
+	hidden_dim = 6
+	cell_dim = 6
+
+	machine = ner(embedding_dim, hidden_dim, cell_dim, len(word_to_ix), len(tag_to_ix), learning_rate = 0.1, minibatch_size = 1, max_epoch = 300, train_data = training_data_in_index)
+
+	machine.train()
 
 
 
 
 
+
+
+
+if __name__ == "__main__":
+	main()
