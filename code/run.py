4e16daa5cabe88e7a04f445059c23fa92519c88b
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-03-29 12:20:48 -0400

    .

--- code/run.py
+++ code/run.py
@@ -129,7 +129,7 @@ def main():
 
   gpu = True
 
-  learning_rate = 0.001
+  learning_rate = 0.0001
 
   machine = ner(word_embedding_dim, hidden_dim, label_embedding_dim, vocab_size, label_size, learning_rate=learning_rate, minibatch_size=32, max_epoch=max_epoch, train_X=train_X, train_Y=train_Y, test_X=val_X, test_Y=val_Y, attention=attention, gpu=gpu, pretrained=pretrained)
   if gpu:
