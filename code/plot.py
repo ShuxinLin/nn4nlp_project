5a5b4829021958eb952f38de800e5e35269b36bd
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-03-30 21:26:20 -0400

    .

--- code/plot.py
+++ code/plot.py
@@ -6,18 +6,22 @@ import numpy as np
 import re
 
 
-logfile = open("../result_lrn_0p001/log.txt", "r")
-
-epoch = list(range(len(train_loss)))
+logfile = open("../gitignore/result_lrn_0p001_beam_3/log.txt", "r")
 
 train_loss = []
 val_loss = []
+
+train_f = []
+val_f = []
 
 for line in logfile:
   tmp = re.split("\t", line)
   train_loss.append(float(tmp[2]))
   val_loss.append(float(tmp[3]))
+  train_f.append(float(tmp[4]))
+  val_f.append(float(tmp[5]))
 
+epoch = list(range(len(train_loss)))
 
 plt.figure(1)
 plt.plot(epoch, train_loss, "r-", epoch, val_loss, "b-")
@@ -25,9 +29,30 @@ plt.plot(epoch, train_loss, "r-", epoch, val_loss, "b-")
 #plt.ylim([0, 0.5])
 plt.xlabel('Epoch')
 plt.ylabel('Average cross-entropy error')
-plt.savefig('fig_lrn_0p001.pdf')
+plt.savefig('../gitignore/result_lrn_0p001_beam_3/fig_lrn_0p001_beam_3_loss.pdf')
 
 
 
+plt.figure(2)
+plt.plot(epoch, train_f, "r-", epoch, val_f, "b-")
+#plt.xlim([0, 11])
+#plt.ylim([0, 0.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score')
+plt.savefig('../gitignore/result_lrn_0p001_beam_3/fig_lrn_0p001_beam_3_f.pdf')
+
+
+
+
+
+
+
+
+
+
+
+
+
+logfile.close()
 
 
