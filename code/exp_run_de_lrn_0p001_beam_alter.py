f86d61f7bdfdda58e3edfa9c876bb85e6d374e6b
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-04-22 00:12:37 -0400

    Adpating to new master branch merge...

--- code/exp_run_de_lrn_0p001_beam_alter.py
+++ code/exp_run_de_lrn_0p001_beam_alter.py
@@ -11,6 +11,9 @@ from operator import itemgetter
 import collections
 from ner import ner
 import os
+import time
+
+import torch
 
 
 def get_index2word(dict_file):
@@ -104,6 +107,11 @@ def minibatch_of_one_de(data):
 
 
 def main():
+  rnd_seed = None
+  if rnd_seed:
+    torch.manual_seed(rnd_seed)
+    np.random.seed(rnd_seed)
+
   data_path = "../dataset/German/"
 
   result_path = "../result_lrn_0p001_beam_alter/"
@@ -124,17 +132,13 @@ def main():
   val_X, val_Y = minibatch_of_one_de('valid')
   test_X, test_Y = minibatch_of_one_de('test')
 
-  #print("val_X len=",len(val_X))
-  #print("val_X[1000]=",val_X[1000])
-  #print("val_Y[1000]=",val_Y[1000])
-
   # Using word2vec pre-trained embedding
   word_embedding_dim = 300
 
   hidden_dim = 64
   label_embedding_dim = 8
 
-  max_epoch = 200
+  max_epoch = 3
 
   # 0.001 is a good value
   learning_rate = 0.001
@@ -148,37 +152,120 @@ def main():
     word_embedding_dim = 64
 
   gpu = True
+  if gpu and rnd_seed:
+    torch.cuda.manual_seed(rnd_seed)
 
-  machine = ner(word_embedding_dim, hidden_dim, label_embedding_dim, vocab_size, label_size, learning_rate=learning_rate, minibatch_size=batch_size, max_epoch=max_epoch, train_X=train_X, train_Y=train_Y, val_X=val_X, val_Y=val_Y, test_X=test_X, test_Y=test_Y, attention=attention, gpu=gpu, pretrained=pretrained)
+  load_model_filename = None
+
+  machine = ner(word_embedding_dim, hidden_dim, label_embedding_dim, vocab_size, label_size, learning_rate=learning_rate, minibatch_size=batch_size, max_epoch=max_epoch, train_X=train_X, train_Y=train_Y, val_X=val_X, val_Y=val_Y, test_X=test_X, test_Y=test_Y, attention=attention, gpu=gpu, pretrained=pretrained, load_model_filename=load_model_filename)
   if gpu:
     machine = machine.cuda()
 
-  # "beam_size = 0" will use greedy
-  # "beam_size = 1" will still use beam search, just with beam size = 1
-  beam_size = 1
-
   shuffle = True
 
-  train_loss_list = machine.train(shuffle, beam_size, result_path)
+  # Pure training, no evaluation
+  train_loss_list = machine.train(shuffle, result_path, False, None)
+
+
+  ##################
+
+  #eval_output_file_greedy = open(os.path.join(result_path, "eval_greedy.txt"), "w+")
+  eval_output_file_beam_1 = open(os.path.join(result_path, "eval_beam_1.txt"), "w+")
+  eval_output_file_beam_2 = open(os.path.join(result_path, "eval_beam_2.txt"), "w+")
+  eval_output_file_beam_3 = open(os.path.join(result_path, "eval_beam_3.txt"), "w+")
+
+  for epoch in range(0, max_epoch):
+    load_model_filename = os.path.join(result_path, "ckpt_" + str(epoch) + ".pth")
+
+    machine = ner(word_embedding_dim, hidden_dim, label_embedding_dim, vocab_size, label_size, learning_rate=learning_rate, minibatch_size=batch_size, max_epoch=max_epoch, train_X=train_X, train_Y=train_Y, val_X=val_X, val_Y=val_Y, test_X=test_X, test_Y=test_Y, attention=attention, gpu=gpu, pretrained=pretrained, load_model_filename=load_model_filename)
+    if gpu:
+      machine = machine.cuda()
+
+    # "beam_size = 0" will use greedy
+    # "beam_size = 1" will still use beam search, just with beam size = 1
+    beam_size = 1
+
+    #train_loss_greedy, train_fscore_greedy = machine.evaluate(train_X, train_Y, index2word, index2label, "train", None, beam_size)
+    val_loss_beam_1, val_fscore_beam_1 = machine.evaluate(val_X, val_Y, index2word, index2label, "val", None, beam_size)
+
+    time_begin_beam_1 = time.time()
+    test_loss_beam_1, test_fscore_beam_1 = machine.evaluate(test_X, test_Y, index2word, index2label, "test", None, beam_size)
+    time_end_beam_1 = time.time()
+
+    ###
+    beam_size = 2
+
+    #train_loss_beam_1, train_fscore_beam_1 = machine.evaluate(train_X, train_Y, index2word, index2label, "train", None, beam_size)
+    val_loss_beam_2, val_fscore_beam_2 = machine.evaluate(val_X, val_Y, index2word, index2label, "val", None, beam_size)
+
+    time_begin_beam_2 = time.time()
+    test_loss_beam_2, test_fscore_beam_2 = machine.evaluate(test_X, test_Y, index2word, index2label, "test", None, beam_size)
+    time_end_beam_2 = time.time()
+
+
+    ###
+    beam_size = 3
+
+    #train_loss_beam_3, train_fscore_beam_3 = machine.evaluate(train_X, train_Y, index2word, index2label, "train", None, beam_size)
+    val_loss_beam_3, val_fscore_beam_3 = machine.evaluate(val_X, val_Y, index2word, index2label, "val", None, beam_size)
+
+    time_begin_beam_3 = time.time()
+    test_loss_beam_3, test_fscore_beam_3 = machine.evaluate(test_X, test_Y, index2word, index2label, "test", None, beam_size)
+    time_end_beam_3 = time.time()
+
+
+    print("epoch %d" % epoch)
+    print("Beam size 1\n"
+          #"training loss = %.6f" % train_loss_beam_1,
+          "validation loss = %.6f" % val_loss_beam_1,
+          ", test loss = %.6f\n" % test_loss_beam_1,
+          #"training F score = %.6f" % train_fscore_beam_1,
+          "validation F score = %.6f" % val_fscore_beam_1,
+          ", test F score = %.6f\n" % test_fscore_beam_1,
+          "test time = %.6f" % (time_end_beam_1 - time_begin_beam_1))
+    print("Beam size 2\n"
+          #"training loss = %.6f" % train_loss_beam_1,
+          "validation loss = %.6f" % val_loss_beam_2,
+          ", test loss = %.6f\n" % test_loss_beam_2,
+          #"training F score = %.6f" % train_fscore_beam_1,
+          "validation F score = %.6f" % val_fscore_beam_2,
+          ", test F score = %.6f\n" % test_fscore_beam_2,
+          "test time = %.6f" % (time_end_beam_1 - time_begin_beam_1))
+    print("Beam size 3\n"
+          #"training loss = %.6f" % train_loss_beam_3,
+          "validation loss = %.6f" % val_loss_beam_3,
+          ", test loss = %.6f\n" % test_loss_beam_3,
+          #"training F score = %.6f" % train_fscore_beam_3,
+          "validation F score = %.6f" % val_fscore_beam_3,
+          ", test F score = %.6f\n" % test_fscore_beam_3,
+          "test time = %.6f" % (time_end_beam_3 - time_begin_beam_3))
+
+    eval_output_file_beam_1.write("%d\t%f\t%f\t%f\t%f\t%f\n" % (epoch, val_loss_beam_1, test_loss_beam_1, val_fscore_beam_1, test_fscore_beam_1, time_end_beam_1 - time_begin_beam_1))
+    eval_output_file_beam_1.flush()
+
+    eval_output_file_beam_2.write("%d\t%f\t%f\t%f\t%f\t%f\n" % (epoch, val_loss_beam_2, test_loss_beam_2, val_fscore_beam_2, test_fscore_beam_2, time_end_beam_2 - time_begin_beam_2))
+    eval_output_file_beam_2.flush()
+
+    eval_output_file_beam_3.write("%d\t%f\t%f\t%f\t%f\t%f\n" % (epoch, val_loss_beam_3, test_loss_beam_3, val_fscore_beam_3, test_fscore_beam_3, time_end_beam_3 - time_begin_beam_3))
+    eval_output_file_beam_3.flush()
+  # End for epoch
+
+  eval_output_file_beam_1.close()
+  eval_output_file_beam_2.close()
+  eval_output_file_beam_3.close()
+
+
+
+
+
+
+
+
   # Write out files
   #train_eval_loss, train_eval_fscore = machine.evaluate(train_X, train_Y, index2word, index2label, "train", result_path, beam_size)
-  val_eval_loss, val_eval_fscore = machine.evaluate(val_X, val_Y, index2word, index2label, "val", result_path, beam_size)
-  test_eval_loss, test_eval_fscore = machine.evaluate(test_X, test_Y, index2word, index2label, "test", result_path, beam_size)
+  #val_eval_loss, val_eval_fscore = machine.evaluate(val_X, val_Y, index2word, index2label, "val", result_path, beam_size)
+  #test_eval_loss, test_eval_fscore = machine.evaluate(test_X, test_Y, index2word, index2label, "test", result_path, beam_size)
 
-  #print("train_eval_loss =", train_eval_loss)
-  #print("val_eval_loss =", val_eval_loss)
-
-  #print(train_loss_list)
-
-  """
-  plt.figure(1)
-  plt.plot(list(range(len(train_loss_list))) , train_loss_list, "k-")
-  #plt.xlim([0, 11])
-  #plt.ylim([0, 0.5])
-  plt.xlabel("Epoch")
-  plt.ylabel("Cross-entropy loss")
-  plt.savefig(result_path + "fig_exp1.pdf")
-  """
 
 if __name__ == "__main__":
   main()
