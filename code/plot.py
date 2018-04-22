9addf616c0297a3216777621ed4c47bd5a06417d
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-04-21 20:43:00 -0400

    New plot code

--- code/plot.py
+++ code/plot.py
@@ -6,6 +6,137 @@ import numpy as np
 import re
 
 
+logfile = open("../gitignore/result_lrn_0p001/eval_greedy.txt", "r")
+
+train_f = []
+val_f = []
+test_f = []
+greedy_test_f = []
+
+for line in logfile:
+  tmp = re.split("\t", line)
+  train_f.append(float(tmp[4]))
+  val_f.append(float(tmp[5]))
+  test_f.append(float(tmp[6]))
+  greedy_test_f.append(float(tmp[6]))
+
+epoch = list(range(len(train_f)))
+
+epochToPlot = len(train_f)
+
+plt.figure(1)
+plt.plot(epoch[:epochToPlot], train_f[:epochToPlot], "r-", epoch[:epochToPlot], val_f[:epochToPlot], "g-", epoch[:epochToPlot], test_f[:epochToPlot], "b-")
+#plt.xlim([0, 11])
+#plt.ylim([0, 0.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score')
+plt.savefig('../gitignore/result_lrn_0p001/fig_lrn_0p001_greedy_f.pdf')
+
+
+logfile.close()
+
+
+
+
+
+logfile = open("../gitignore/result_lrn_0p001/eval_beam_1.txt", "r")
+
+train_f = []
+val_f = []
+test_f = []
+beam_1_test_f = []
+
+for line in logfile:
+  tmp = re.split("\t", line)
+  train_f.append(float(tmp[4]))
+  val_f.append(float(tmp[5]))
+  test_f.append(float(tmp[6]))
+  beam_1_test_f.append(float(tmp[6]))
+
+epoch = list(range(len(train_f)))
+
+epochToPlot = len(train_f)
+
+plt.figure(2)
+plt.plot(epoch[:epochToPlot], train_f[:epochToPlot], "r-", epoch[:epochToPlot], val_f[:epochToPlot], "g-", epoch[:epochToPlot], test_f[:epochToPlot], "b-")
+#plt.xlim([0, 11])
+#plt.ylim([0, 0.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score')
+plt.savefig('../gitignore/result_lrn_0p001/fig_lrn_0p001_beam_1_f.pdf')
+
+
+logfile.close()
+
+
+
+
+
+
+
+logfile = open("../gitignore/result_lrn_0p001/eval_beam_3.txt", "r")
+
+train_f = []
+val_f = []
+test_f = []
+beam_3_test_f = []
+
+for line in logfile:
+  tmp = re.split("\t", line)
+  train_f.append(float(tmp[4]))
+  val_f.append(float(tmp[5]))
+  test_f.append(float(tmp[6]))
+  beam_3_test_f.append(float(tmp[6]))
+
+epoch = list(range(len(train_f)))
+
+epochToPlot = len(train_f)
+
+plt.figure(3)
+plt.plot(epoch[:epochToPlot], train_f[:epochToPlot], "r-", epoch[:epochToPlot], val_f[:epochToPlot], "g-", epoch[:epochToPlot], test_f[:epochToPlot], "b-")
+#plt.xlim([0, 11])
+#plt.ylim([0, 0.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score')
+plt.savefig('../gitignore/result_lrn_0p001/fig_lrn_0p001_beam_3_f.pdf')
+
+
+logfile.close()
+
+
+
+
+
+plt.figure(4)
+#plt.plot(epoch[:epochToPlot], greedy_test_f[:epochToPlot], "r^", epoch[:epochToPlot], beam_1_test_f[:epochToPlot], "g-", epoch[:epochToPlot], beam_3_test_f[:epochToPlot], "bo")
+plt.plot(epoch[:epochToPlot], greedy_test_f[:epochToPlot], "r-", epoch[:epochToPlot], beam_3_test_f[:epochToPlot], "b-")
+#plt.xlim([0, 11])
+#plt.ylim([0, 0.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score')
+plt.savefig('../gitignore/result_lrn_0p001/fig_lrn_0p001_combine_f.pdf')
+
+
+
+diff = [(beam_3_test_f[i] - greedy_test_f[i]) for i in range(len(greedy_test_f))]
+
+plt.figure(5)
+plt.plot(epoch[:epochToPlot], diff[:epochToPlot], "b-")
+plt.plot(epoch[:epochToPlot], np.zeros(epochToPlot), "k--")
+#plt.xlim([0, 11])
+plt.ylim([-2.5, 2.5])
+plt.xlabel('Epoch')
+plt.ylabel('F-score diff (beam_3 - greedy)')
+plt.savefig('../gitignore/result_lrn_0p001/fig_lrn_0p001_diff_f.pdf')
+
+
+
+
+
+
+
+
+"""
 logfile = open("../gitignore/result_lrn_0p001_beam_10/log.txt", "r")
 
 train_loss = []
@@ -48,17 +179,6 @@ plt.ylabel('F-score')
 plt.savefig('../gitignore/result_lrn_0p001_beam_10/fig_lrn_0p001_beam_10_f.pdf')
 
 
-
-
-
-
-
-
-
-
-
-
-
 logfile.close()
-
+"""
 
