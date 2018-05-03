2f20a95f9df105b4b7657b2f987a8c394a0da2c0
Hai <htpham@andrew.cmu.edu>
2018-05-02 20:26:11 -0400

    Change the model to MLP

--- code/rl/model.py
+++ code/rl/model.py
@@ -29,47 +29,6 @@ def weights_init(m):
     m.bias.data.fill_(0)
 
 
-class ActorCritic(torch.nn.Module):
-  def __init__(self, num_inputs, action_space):
-    super(ActorCritic, self).__init__()
-    self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
-    self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-    self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-    self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
-
-    self.lstm = nn.LSTMCell(32 * 3 * 3, 256)
-
-    num_outputs = action_space.n
-    self.critic_linear = nn.Linear(256, 1)
-    self.actor_linear = nn.Linear(256, num_outputs)
-
-    self.apply(weights_init)
-    self.actor_linear.weight.data = normalized_columns_initializer(
-      self.actor_linear.weight.data, 0.01)
-    self.actor_linear.bias.data.fill_(0)
-    self.critic_linear.weight.data = normalized_columns_initializer(
-      self.critic_linear.weight.data, 1.0)
-    self.critic_linear.bias.data.fill_(0)
-
-    self.lstm.bias_ih.data.fill_(0)
-    self.lstm.bias_hh.data.fill_(0)
-
-    self.train()
-
-  def forward(self, inputs):
-    inputs, (hx, cx) = inputs
-    x = F.elu(self.conv1(inputs))
-    x = F.elu(self.conv2(x))
-    x = F.elu(self.conv3(x))
-    x = F.elu(self.conv4(x))
-
-    x = x.view(-1, 32 * 3 * 3)
-    hx, cx = self.lstm(x, (hx, cx))
-    x = hx
-
-    return self.critic_linear(x), self.actor_linear(x), (hx, cx)
-
-
 class AdaptiveActorCritic(torch.nn.Module):
   def __init__(self, max_beam_size, action_space=3):
     """ Format of a state (a vector): 
@@ -81,60 +40,16 @@ class AdaptiveActorCritic(torch.nn.Module):
         action_space: number of possible actions, default is 3 
     """
     super(AdaptiveActorCritic, self).__init__()
-    self.max_beam_size = max_beam_size
 
-    # RNN layer - for the whole sequence
-    hidden_size = 32
-    self.logp_lstm = nn.LSTM(max_beam_size, hidden_size)
-    self.accum_lstm = nn.LSTM(max_beam_size, hidden_size)
-
-    self.critic_linear = nn.Linear(hidden_size*2 + 1, 1)
-    self.actor_linear = nn.Linear(hidden_size * 2 + 1, action_space)
-
-    # # FC layers
-    # hidden_size = max_beam_size * 2 + 1
-    # self.critic_linear = nn.Linear(hidden_size, 1)
-    # self.actor_linear = nn.Linear(hidden_size, action_space)
+    # FC layers
+    hidden_size = max_beam_size * 2 + 1
+    self.critic_linear = nn.Linear(hidden_size, 1)
+    self.actor_linear = nn.Linear(hidden_size, action_space)
 
   def forward(self, state):
-    state = state.astype(np.float32)
+    state = Variable(torch.FloatTensor(state))
 
-    accum_logP = state[0:self.max_beam_size].reshape(1, 1, -1)
-    logP = state[self.max_beam_size:2 * self.max_beam_size].reshape(1, 1, -1)
-    beam_size = state[-1]
-
-    # to Variable
-    accum_logP = Variable(torch.FloatTensor(accum_logP)).view(1, 1, -1)
-    logP = Variable(torch.FloatTensor(logP)).view(1, 1, -1)
-    beam_size = Variable(torch.FloatTensor([int(beam_size)])).view(1, -1)
-
-
-    # LSTM now
-    h1, c1 = self.logp_lstm(accum_logP)
-    h2, c2 = self.logp_lstm(logP)
-
-    h1 = h1.view(1, -1)
-    h2 = h2.view(1, -1)
-    cat_output = torch.cat([h1, h2, beam_size], dim=1) # 1 x (2H + 1)
-
-    # # concatenate 2 scores
-    # cat_scores = np.concatenate([accum_logP, logP], axis=1)
-    # print(cat_scores.shape)
-    #
-    # beam_size_in = int(state[2 * self.max_beam_size])
-    #
-    # # to variables
-    # accum_logP = Variable(torch.FloatTensor(accum_logP))
-    # logP = Variable(torch.FloatTensor(logP))
-    #
-    # hx, cx = self.logp_lstm(accum_logP)
-    # print(hx)
-    # print(cx)
-
-    # TODO: turn to RNN
-    # state = Variable(torch.FloatTensor(state))
-
-    return self.critic_linear(cat_output), self.actor_linear(cat_output)
+    return self.critic_linear(state), self.actor_linear(state)
 
 
 if __name__ == "__main__":
