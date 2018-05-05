5d1028c3505f6af31baf5ca2497dfca6db3f0a0e
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-05-05 14:05:11 -0400

    .

--- code/ner.py
+++ code/ner.py
@@ -1371,6 +1371,7 @@ class ner(nn.Module):
     # End for t
 
     if self.attention:
+      print(attention_pred_seq)
       attention_pred_seq = torch.stack(attention_pred_seq, dim = 0)
     else:
       attention_pred_seq = None
