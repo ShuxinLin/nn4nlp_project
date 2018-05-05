041bb901aaf8a3d4de0f0c12803d9ad47983cfd2
Yu-Hsiang Lin <hitr2997925@gmail.com>
2018-05-05 14:47:21 -0400

    .

--- code/ner_multigpu.py
+++ code/ner_multigpu.py
@@ -1377,6 +1377,7 @@ class ner(nn.Module):
     if self.attention:
       # BUG: to fix later
       #attention_pred_seq = torch.stack(attention_pred_seq, dim = 0)
+      pass
     else:
       attention_pred_seq = None
 
